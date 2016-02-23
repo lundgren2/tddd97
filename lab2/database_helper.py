3891d62841b41574847bcc62fac7bf5506321601
Tobias Lundgren <tobias.l@me.com>
2016-02-23 23:53:24 +0100

    added SQLite with Flask

--- lab2/database_helper.py
+++ lab2/database_helper.py
@@ -1,1 +1,16 @@
-olakwd
+import sqlite3
+from flask import g
+
+DATABASE = '/path/to/database.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = connect_to_database()
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
--- lab2/server.py
+++ lab2/server.py
@@ -1,1 +1,19 @@
-olakwd
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+@app.route("/sign_in")
+def login();
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run()
