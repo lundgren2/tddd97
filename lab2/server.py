8c174cf5a03369e6edab01aae2955b3a58f766e5
Tobias Lundgren <tobias.l@me.com>
2016-03-10 15:32:11 +0100

    Fortsatt med server.py

--- lab2/server.py
+++ lab2/server.py
@@ -1,14 +1,49 @@
+from flask import app, request
 from flask import Flask
+import database_helper
+import json
+
+
+
+#Lesson 2
+
 app = Flask(__name__)
+app.debug = True
+
+@app.before_request
+def before_request():
+    database_helper.connect_db()
+
+@app.teardownrequest
+def teardown_request(exception):
+    database_helper.close_db()
+
+
+
 
 @app.route("/")
-def hello():
+def hello_world():
     return "Hello World!"
 
 
-@app.route("/sign_in/<email>/<password>")
-def login(email,password):
+@app.route('/signup', methods=['POST'])
+def signup():
 
+    email = request.get_json()['email']
+    password = request.get_json()['password']
+    firstname = request.get_json()['firstname']
+    familyname = request.get_json()['familyname']
+    gender = request.get_json()['gender']
+    city = request.get_json()['city']
+    country = request.get_json()['country']
+    #messages = ???
+
+
+
+@app.route('/sign_in/<email>/<password>', methods=['GET'])
+def login(email, password):
+
+   # result = database_helper.get_contact(email, password)
     email = request.form['email']
     password = request.form['password']
 
